aaf9284eac95740c2f1e9d8eb3cd5265e7f10dc1
Matheus DF <51722889+matheusdf32@users.noreply.github.com>
2020-09-09 21:49:31 -0300

    teste com viado

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ from PIL import Image
 # alt u hides hud ingmae
 def captureScreen():  # BeamNG.drive - 0.20.2.0.10611 - RELEASE - x64
     printscreen = np.array(ImageGrab.grab(bbox=(62, 40, 960, 540)))
-    # cv2.imshow('window', cv2.cvtColor(printscreen, cv2.COLOR_BGR2RGB))
-    # cv2.waitKey(1)
+    cv2.imshow('window', cv2.cvtColor(printscreen, cv2.COLOR_BGR2RGB))
+    cv2.waitKey(1)
     return printscreen
 
 
@@ -42,21 +42,25 @@ def main():
     while 1:
         toSend = {}
         frameQueue += 1
+        # data = conn.recv(128)
+        # data = str(data, 'utf-8')
+        # print("received data: ", data)  # steering_input
+        # conn.send((json.dumps(toSend) + '\n\r').encode('utf-8'))
         img = captureScreen()
-        if img.any():
-            data = conn.recv(128)
-            data = str(data, 'utf-8')
-            data = re.search('\n(.+?)\n', data)
-            if (data):
-                data = json.loads(data.group(1))
-            if not isinstance(data, str):
-                # data['steering_input'] = 1
-                print("received data: ", data)  # steering_input
-                print('running at {} fps'.format(1 / (time.time() - last_time)) + ' queue: ' + str(frameQueue))
-                frameQueue -= 1
+        data = conn.recv(128)
+        if not img.any(): pass
+        data = str(data, 'utf-8')
+        data = re.search('\n(.+?)\n', data)
+        if (data):
+            data = json.loads(data.group(1))
+        if not isinstance(data, str):
+            # data['steering_input'] = 1
+            print("received data: ", data)  # steering_input
+            print('running at {} fps'.format(1 / (time.time() - last_time)) + ' queue: ' + str(frameQueue))
+            frameQueue -= 1
 
-            # conn.send((json.dumps(toSend) + '\n\r').encode('utf-8'))  # echo
-            last_time = time.time()
+        # conn.send((json.dumps(toSend) + '\n\r').encode('utf-8'))  # echo
+        last_time = time.time()
     conn.close()
 
 
