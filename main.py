d2e63beee266bac2555d46339c86bc2a636daa9e
Matheus DF <51722889+matheusdf32@users.noreply.github.com>
2020-10-29 22:44:15 -0300

    binarizacao estranha

--- main.py
+++ main.py
@@ -47,6 +47,42 @@ def getData(conn):
         data = json.loads(data)
         # print(data)
 
+def process_binary(img):
+    """ Process image to generate a sanitized binary image
+    Args:
+        img: undistorted image in BGR
+    Returns:
+        Binary image
+    """
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0)
+    abs_sobelx = np.absolute(sobelx)
+    scaled_sobel = np.uint8(255 * abs_sobelx / np.max(abs_sobelx))
+    sxbinary = np.zeros_like(scaled_sobel)
+    retval, sxthresh = cv2.threshold(scaled_sobel, 30, 150, cv2.THRESH_BINARY)
+    sxbinary[(sxthresh >= 30) & (sxthresh <= 150)] = 1
+
+    hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+    s_channel = hls[:, :, 2]
+    s_binary = np.zeros_like(s_channel)
+    # cv2.inRange sets 255 if in range other wise 0
+    s_thresh = cv2.inRange(s_channel.astype('uint8'), 175, 250)
+    # set 255 to 1
+    s_binary[(s_thresh == 255)] = 1
+
+    combined_binary = np.zeros_like(gray)
+    combined_binary[(s_binary == 1) | (sxbinary == 1)] = 1
+
+    return combined_binary
+
+def do_canny(frame):
+    # Converts frame to grayscale because we only need the luminance channel for detecting edges - less computationally expensive
+    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    # Applies a 5x5 gaussian blur with deviation of 0 to frame - not mandatory since Canny will do this for us
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    # Applies Canny edge detector with minVal of 50 and maxVal of 150
+    canny = cv2.Canny(blur, 50, 150)
+    return canny
 
 def main():
     loop = asyncio.get_event_loop()
@@ -63,7 +99,7 @@ def main():
     frameQueue = 0
 
     width, height = 960, 540
-    pts1 = np.float32([[323, 238], [581, 219], [89, 390], [835, 394]])
+    pts1 = np.float32([[409, 275], [539, 274], [122, 393], [757, 394]])
     pts2 = np.float32([[0, 0], [width, 0], [0, height], [width, height]])
     matrix = cv2.getPerspectiveTransform(pts1, pts2)
 
@@ -72,9 +108,12 @@ def main():
         frameQueue += 1
         img = d.screenshot(region=(62, 40, 960, 540))
         # img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        imgOut = do_canny(img)
+        imgOut = cv2.warpPerspective(imgOut, matrix, (width, height))
+
         # imageShow(img)
-        imgOut = cv2.warpPerspective(img, matrix, (width, height))
+
         imageShow(imgOut)
 
         print("throttle_input: " + '{0:,.2f}'.format(data["throttle_input"])
